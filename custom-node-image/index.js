78669a82fee6f84016b8548383a92789033c4373
Avijeet Pandey <avijeet_official@outlook.com>
2020-11-25 21:41:24 +0530

    updated index

--- custom-node-image/index.js
+++ custom-node-image/index.js
@@ -6,6 +6,12 @@ app.get("/",(req,res)=>{
     res.send("<h1>Visiting Root</h1>")
 })
 
+app.get("/login",(req,res)=>{
+    res.json({
+        message : "You are logged In"
+    })
+})
+
 app.listen(3000,()=>{
     console.log('Server is up and running')
 })
